ef95d220960e92604e049be08f24e0c769a7801f
Kota Ramsey <66634314+Jeddsh@users.noreply.github.com>
2024-02-29 01:17:32 -0500

    added small skeleton to gpt_integration.py

--- src/gpt_integration.py
+++ src/gpt_integration.py
@@ -1,6 +1,16 @@
 # gpt_integration.py
+import os
+import openai
+
 
 class GPTAPI:
+    """Creates a Chat bot so that code snipets for AI models"""
+
+    # todo: set up openai api key
+    openai.api_key = None
+
+    # model of openai that the chat bot will use
+    Model_Name = "gpt-4"
     pass
 
 def call_gpt():
@@ -11,4 +21,3 @@ def handle_gpt_response():
 
 def integrate_generated_code():
     pass
-
