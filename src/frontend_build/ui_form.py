d8e2a09c51060a632761e00a4e05146c2daec2cd
TimBOgg <to.donessgg@gmail.com>
2024-03-03 13:15:05 -0500

    Edited Frontend Layout

--- src/frontend_build/ui_form.py
+++ src/frontend_build/ui_form.py
@@ -15,9 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QColumnView, QFrame, QGridLayout,
-    QMainWindow, QPushButton, QScrollArea, QSizePolicy,
-    QStackedWidget, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QDockWidget, QFrame, QGridLayout,
+    QMainWindow, QPushButton, QSizePolicy, QVBoxLayout,
+    QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -25,8 +25,7 @@ class Ui_MainWindow(object):
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.resize(914, 699)
         MainWindow.setMaximumSize(QSize(1920, 1080))
-        MainWindow.setStyleSheet(u"\n"
-"background-color: rgb(225, 255, 247);")
+        MainWindow.setStyleSheet(u"alternate-background-color: rgb(255, 255, 255);")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout = QGridLayout(self.centralwidget)
@@ -35,50 +34,27 @@ class Ui_MainWindow(object):
         self.gridLayout_2.setObjectName(u"gridLayout_2")
         self.frame = QFrame(self.centralwidget)
         self.frame.setObjectName(u"frame")
-        self.frame.setEnabled(True)
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
-        sizePolicy.setHorizontalStretch(1)
-        sizePolicy.setVerticalStretch(1)
-        sizePolicy.setHeightForWidth(self.frame.sizePolicy().hasHeightForWidth())
-        self.frame.setSizePolicy(sizePolicy)
-        self.frame.setMinimumSize(QSize(0, 0))
-        self.frame.setMaximumSize(QSize(862, 628))
-        self.frame.setSizeIncrement(QSize(1, 1))
-        self.frame.setStyleSheet(u"background-color: rgb(255, 255, 255);")
-        self.gridLayout_4 = QGridLayout(self.frame)
-        self.gridLayout_4.setObjectName(u"gridLayout_4")
-        self.gridLayout_3 = QGridLayout()
-        self.gridLayout_3.setObjectName(u"gridLayout_3")
-        self.frame_2 = QFrame(self.frame)
-        self.frame_2.setObjectName(u"frame_2")
-        self.frame_2.setMinimumSize(QSize(139, 608))
-        self.frame_2.setMaximumSize(QSize(16777215, 608))
-        self.frame_2.setStyleSheet(u"background-color: rgb(255, 255, 127);")
-        self.frame_2.setFrameShape(QFrame.StyledPanel)
-        self.frame_2.setFrameShadow(QFrame.Raised)
-        self.horizontalLayoutWidget_2 = QWidget(self.frame_2)
-        self.horizontalLayoutWidget_2.setObjectName(u"horizontalLayoutWidget_2")
-        self.horizontalLayoutWidget_2.setGeometry(QRect(0, 0, 151, 611))
-        self.gridLayout_5 = QGridLayout(self.horizontalLayoutWidget_2)
-        self.gridLayout_5.setObjectName(u"gridLayout_5")
-        self.gridLayout_5.setContentsMargins(0, 0, 0, 0)
-        self.frame_3 = QFrame(self.horizontalLayoutWidget_2)
-        self.frame_3.setObjectName(u"frame_3")
-        self.frame_3.setMaximumSize(QSize(169, 619))
-        self.frame_3.setStyleSheet(u"background-color: rgb(255, 170, 0);")
-        self.frame_3.setFrameShape(QFrame.StyledPanel)
-        self.frame_3.setFrameShadow(QFrame.Raised)
-        self.verticalLayoutWidget = QWidget(self.frame_3)
+        self.frame.setStyleSheet(u"background-color: rgb(119, 118, 123);")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+
+        self.gridLayout_2.addWidget(self.frame, 0, 0, 1, 1)
+
+
+        self.gridLayout.addLayout(self.gridLayout_2, 0, 0, 1, 1)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.dockWidget = QDockWidget(MainWindow)
+        self.dockWidget.setObjectName(u"dockWidget")
+        self.dockWidget.setStyleSheet(u"background-color: rgb(153, 193, 241);")
+        self.dockWidgetContents = QWidget()
+        self.dockWidgetContents.setObjectName(u"dockWidgetContents")
+        self.verticalLayoutWidget = QWidget(self.dockWidgetContents)
         self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
-        self.verticalLayoutWidget.setGeometry(QRect(20, 130, 101, 331))
+        self.verticalLayoutWidget.setGeometry(QRect(20, 40, 121, 151))
         self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.pushButton = QPushButton(self.verticalLayoutWidget)
-        self.pushButton.setObjectName(u"pushButton")
-
-        self.verticalLayout.addWidget(self.pushButton)
-
         self.pushButton_2 = QPushButton(self.verticalLayoutWidget)
         self.pushButton_2.setObjectName(u"pushButton_2")
 
@@ -89,76 +65,23 @@ class Ui_MainWindow(object):
 
         self.verticalLayout.addWidget(self.pushButton_3)
 
+        self.pushButton = QPushButton(self.verticalLayoutWidget)
+        self.pushButton.setObjectName(u"pushButton")
 
-        self.gridLayout_5.addWidget(self.frame_3, 0, 1, 1, 1)
+        self.verticalLayout.addWidget(self.pushButton)
 
-        self.stackedWidget = QStackedWidget(self.frame_2)
-        self.stackedWidget.setObjectName(u"stackedWidget")
-        self.stackedWidget.setGeometry(QRect(210, 0, 631, 611))
-        self.page = QWidget()
-        self.page.setObjectName(u"page")
-        self.frame_4 = QFrame(self.page)
-        self.frame_4.setObjectName(u"frame_4")
-        self.frame_4.setGeometry(QRect(160, 200, 120, 80))
-        self.frame_4.setStyleSheet(u"background-color: rgb(170, 255, 127);")
-        self.frame_4.setFrameShape(QFrame.StyledPanel)
-        self.frame_4.setFrameShadow(QFrame.Raised)
-        self.stackedWidget.addWidget(self.page)
-        self.page_3 = QWidget()
-        self.page_3.setObjectName(u"page_3")
-        self.frame_5 = QFrame(self.page_3)
-        self.frame_5.setObjectName(u"frame_5")
-        self.frame_5.setGeometry(QRect(190, 230, 120, 80))
-        self.frame_5.setStyleSheet(u"background-color: rgb(0, 170, 127);")
-        self.frame_5.setFrameShape(QFrame.StyledPanel)
-        self.frame_5.setFrameShadow(QFrame.Raised)
-        self.stackedWidget.addWidget(self.page_3)
-        self.page_2 = QWidget()
-        self.page_2.setObjectName(u"page_2")
-        self.columnView = QColumnView(self.page_2)
-        self.columnView.setObjectName(u"columnView")
-        self.columnView.setGeometry(QRect(10, 50, 601, 531))
-        self.scrollArea = QScrollArea(self.page_2)
-        self.scrollArea.setObjectName(u"scrollArea")
-        self.scrollArea.setGeometry(QRect(10, 50, 601, 531))
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollAreaWidgetContents = QWidget()
-        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 599, 529))
-        self.frame_6 = QFrame(self.scrollAreaWidgetContents)
-        self.frame_6.setObjectName(u"frame_6")
-        self.frame_6.setGeometry(QRect(190, 130, 120, 80))
-        self.frame_6.setStyleSheet(u"background-color: rgb(170, 85, 127);")
-        self.frame_6.setFrameShape(QFrame.StyledPanel)
-        self.frame_6.setFrameShadow(QFrame.Raised)
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
-        self.stackedWidget.addWidget(self.page_2)
-
-        self.gridLayout_3.addWidget(self.frame_2, 0, 0, 1, 1)
-
-
-        self.gridLayout_4.addLayout(self.gridLayout_3, 0, 0, 1, 1)
-
-
-        self.gridLayout_2.addWidget(self.frame, 0, 0, 1, 1)
-
-
-        self.gridLayout.addLayout(self.gridLayout_2, 0, 0, 1, 1)
-
-        MainWindow.setCentralWidget(self.centralwidget)
+        self.dockWidget.setWidget(self.dockWidgetContents)
+        MainWindow.addDockWidget(Qt.LeftDockWidgetArea, self.dockWidget)
 
         self.retranslateUi(MainWindow)
-
-        self.stackedWidget.setCurrentIndex(2)
-
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Page 1", None))
-        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"Page 2", None))
-        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"Page 3", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
     # retranslateUi
 
