06f51d473955065b6893fc28ad93d2a469a28fd6
REGISTERSALAD <PACOBALTHAZ@gmail.com>
2024-03-14 23:44:29 -0400

    Cmake Stuff Added Branch for model specific UIS Will merge to main when finished

---
+++ src/frontend_build/mainwindow.py
@@ -0,0 +1,64 @@
+# This Python file uses the following encoding: utf-8
+
+# Importing necessary modules
+import sys  # Importing sys module for system-related functionalities
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget, QFileDialog  # Importing necessary PySide6 modules
+from PySide6 import *
+from ui_form import Ui_MainWindow  # Importing the user interface class
+import os
+
+# Calculate the path to the directory containing database.py
+module_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+if module_dir not in sys.path:
+    sys.path.append(module_dir)
+
+# Defining the main window class
+class MainWindow(QMainWindow):
+    fname = None
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_MainWindow()  # Creating an instance of the user interface class
+        self.ui.setupUi(self)  # Setting up the user interface in the main window
+
+        # Connecting button clicks to respective functions
+        self.ui.pushButton.clicked.connect(self.switch_to_page1)  # Connecting pushButton click to switch_to_page1 function
+        self.ui.pushButton_2.clicked.connect(self.switch_to_page2)  # Connecting pushButton_2 click to switch_to_page2 function
+        self.ui.pushButton_3.clicked.connect(self.switch_to_page3)  # Connecting pushButton_3 click to switch_to_page3 function
+        self.ui.pushButton_5.clicked.connect(self.list_file)
+        self.ui.fileList.findChild(QLabel, "fileList")
+
+
+        self.ui.actionOpen_Dock.triggered.connect(self.openDock)
+
+    def openDock(self):
+        self.ui.dockWidget.show()
+
+    # Function to switch to page 1
+    def switch_to_page1(self):
+        self.ui.stackedWidget.setCurrentIndex(0)  # Setting the index of stackedWidget to 0 (page 1)
+
+    # Function to switch to page 2
+    def switch_to_page2(self):
+        self.ui.stackedWidget.setCurrentIndex(1)  # Setting the index of stackedWidget to 1 (page 2)
+
+    # Function to switch to page 3
+    def switch_to_page3(self):
+        self.ui.stackedWidget.setCurrentIndex(2)  # Setting the index of stackedWidget to 2 (page 3)
+
+    def list_file(self, fname):
+        fName = QFileDialog.getOpenFileName(self, "File Opener", "", "All Files (*)") #(self, title, specify starting directory, filter file formats) [ex: any(*);;python(*.py)]
+
+        if fName:
+            self.ui.fileList.setText(str(fName))
+
+
+
+
+
+
+# Entry point of the program
+if __name__ == "__main__":
+    app = QApplication(sys.argv)  # Creating an instance of QApplication
+    widget = MainWindow()  # Creating an instance of MainWindow class
+    widget.show()  # Displaying the main window
+    sys.exit(app.exec())  # Executing the application and exiting when done
---
+++ src/frontend_build/ui_form.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QDockWidget, QFontComboBox, QFrame,
+    QGridLayout, QHBoxLayout, QLineEdit, QMainWindow,
+    QMenu, QMenuBar, QPushButton, QSizePolicy,
+    QStackedWidget, QTextBrowser, QVBoxLayout, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(914, 699)
+        MainWindow.setMaximumSize(QSize(1920, 1080))
+        MainWindow.setStyleSheet(u"alternate-background-color: rgb(255, 255, 255);")
+        self.actionOpen_Dock = QAction(MainWindow)
+        self.actionOpen_Dock.setObjectName(u"actionOpen_Dock")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.gridLayout_2 = QGridLayout()
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.frame = QFrame(self.centralwidget)
+        self.frame.setObjectName(u"frame")
+        self.frame.setStyleSheet(u"background-color: rgb(119, 118, 123);")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+
+        self.gridLayout_2.addWidget(self.frame, 0, 0, 1, 1)
+
+
+        self.gridLayout.addLayout(self.gridLayout_2, 1, 0, 1, 1)
+
+        self.stackedWidget = QStackedWidget(self.centralwidget)
+        self.stackedWidget.setObjectName(u"stackedWidget")
+        self.stackedWidget.setStyleSheet(u"background-color: rgb(222, 221, 218);")
+        self.page = QWidget()
+        self.page.setObjectName(u"page")
+        self.fontComboBox = QFontComboBox(self.page)
+        self.fontComboBox.setObjectName(u"fontComboBox")
+        self.fontComboBox.setGeometry(QRect(60, 50, 231, 41))
+        self.stackedWidget.addWidget(self.page)
+        self.page_3 = QWidget()
+        self.page_3.setObjectName(u"page_3")
+        self.textBrowser = QTextBrowser(self.page_3)
+        self.textBrowser.setObjectName(u"textBrowser")
+        self.textBrowser.setGeometry(QRect(50, 60, 521, 311))
+        self.textBrowser.setStyleSheet(u"background-color: rgb(0, 0, 0);")
+        self.horizontalLayoutWidget = QWidget(self.page_3)
+        self.horizontalLayoutWidget.setObjectName(u"horizontalLayoutWidget")
+        self.horizontalLayoutWidget.setGeometry(QRect(80, 390, 301, 61))
+        self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.lineEdit = QLineEdit(self.horizontalLayoutWidget)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.horizontalLayout.addWidget(self.lineEdit)
+
+        self.pushButton_4 = QPushButton(self.horizontalLayoutWidget)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+
+        self.horizontalLayout.addWidget(self.pushButton_4)
+
+        self.stackedWidget.addWidget(self.page_3)
+        self.page_2 = QWidget()
+        self.page_2.setObjectName(u"page_2")
+        self.stackedWidget.addWidget(self.page_2)
+
+        self.gridLayout.addWidget(self.stackedWidget, 0, 0, 1, 1)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.dockWidget = QDockWidget(MainWindow)
+        self.dockWidget.setObjectName(u"dockWidget")
+        self.dockWidget.setStyleSheet(u"background-color: rgb(153, 193, 241);")
+        self.dockWidgetContents = QWidget()
+        self.dockWidgetContents.setObjectName(u"dockWidgetContents")
+        self.verticalLayoutWidget = QWidget(self.dockWidgetContents)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(20, 40, 121, 151))
+        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.pushButton = QPushButton(self.verticalLayoutWidget)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.verticalLayout.addWidget(self.pushButton)
+
+        self.pushButton_2 = QPushButton(self.verticalLayoutWidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.verticalLayout.addWidget(self.pushButton_2)
+
+        self.pushButton_3 = QPushButton(self.verticalLayoutWidget)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+
+        self.verticalLayout.addWidget(self.pushButton_3)
+
+        self.dockWidget.setWidget(self.dockWidgetContents)
+        MainWindow.addDockWidget(Qt.LeftDockWidgetArea, self.dockWidget)
+        self.menuBar = QMenuBar(MainWindow)
+        self.menuBar.setObjectName(u"menuBar")
+        self.menuBar.setGeometry(QRect(0, 0, 914, 22))
+        self.menuSettings = QMenu(self.menuBar)
+        self.menuSettings.setObjectName(u"menuSettings")
+        MainWindow.setMenuBar(self.menuBar)
+
+        self.menuBar.addAction(self.menuSettings.menuAction())
+        self.menuSettings.addAction(self.actionOpen_Dock)
+
+        self.retranslateUi(MainWindow)
+
+        self.stackedWidget.setCurrentIndex(0)
+
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.actionOpen_Dock.setText(QCoreApplication.translate("MainWindow", u"Open Dock", None))
+        self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.menuSettings.setTitle(QCoreApplication.translate("MainWindow", u"Settings", None))
+    # retranslateUi
+
