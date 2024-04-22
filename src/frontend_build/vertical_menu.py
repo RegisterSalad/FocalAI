f9fbaad70abb04981f7255f7260bcf26d77373f1
TimBOgg <157186345+TimBOgg@users.noreply.github.com>
2024-04-22 01:04:50 -0400

    Update vertical_menu.py

--- src/frontend_build/vertical_menu.py
+++ src/frontend_build/vertical_menu.py
@@ -13,7 +13,22 @@ if module_dir not in sys.path:
     sys.path.append(module_dir)
     
 class VerticalMenu:
+    """
+    Represents a vertical menu system associated with a parent GUI component. This class manages the creation and 
+    behavior of menus, integrating with a styling system to provide theme toggling capabilities.
+
+    Attributes:
+        parent (QWidget): The parent widget to which the menu is attached, typically a main window.
+        styler (Styler): A styling manager used to apply themes and styles to the menu and other components.
+    """
     def __init__(self, parent, styler):
+        """
+        Initializes a new instance of the VerticalMenu with the specified parent and styler.
+
+        Args:
+            parent (QWidget): The parent widget that this menu is part of.
+            styler (Styler): The styling manager that will be used to apply themes and styles to this menu.
+        """
         self.parent = parent
         self.styler = styler
         self.styler.register_component(self)
@@ -21,6 +36,10 @@ class VerticalMenu:
         self.create_menus()
 
     def create_menus(self):
+        """
+        Sets up the menus within the parent's menu bar. This method adds an 'Edit' menu with a 'Preferences' submenu 
+        for toggling dark mode, and a 'My Models' menu with actions related to model management.
+        """
         editMenu = self.parent.menuBar().addMenu("&Edit")
         preferencesMenu = QMenu("Preferences", self.parent)
         darkModeAction = preferencesMenu.addAction("Dark Mode")
@@ -41,6 +60,10 @@ class VerticalMenu:
         pass
 
     def open_model_list(self):
+        """
+        Opens a list of downloaded models. This method is responsible for initializing and displaying a widget 
+        that lists all models currently available to the user.
+        """
         print("showing downloaded models")
         self.model_list = viewModel_widget()
         print("init successful")
@@ -50,14 +73,27 @@ class VerticalMenu:
 
 
 class viewModel_widget(QWidget):
+     """
+    A widget designed to display a list of models that a user has access to, pulling information from a JSON source. 
+    This class initializes the user interface elements necessary to list models in an easily navigable format.
 
+    Attributes:
+        QWidget
+    """
     def __init__(self):
+        """
+        Initializes the viewModel_widget instance, setting up the JSON data handler and initializing the user interface.
+        """
         super().__init__()
         self.JSON = JSONCaller()
         self.initUI()
 
 
     def initUI(self):
+        """
+        Configures the initial user interface for the viewModel_widget, setting up the layout and style of the widget.
+        This includes creating a list widget for displaying model information and applying basic styling to the widget.
+        """
         print("init UI")
         self.setWindowTitle("View My Models")
         
